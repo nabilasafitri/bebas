01a5cae8c5d20ff86dd6dfdf09cb91239a648122
nabila safitri dwi oktav <abilnabilasafitri@gmail.com>
2020-10-27 09:39:12 +0700

    receive

--- coba_aws.ino
+++ coba_aws.ino
@@ -6,7 +6,7 @@
 #define WIFI_PASSWD "prodigy123" // Password WiFi
 
 #define CLIENT_ID "hello"// thing ID, harus unik/berbeda dari setiap thing yang terkoneksi ke AWS account
-#define MQTT_TOPIC "$aws/things/hello/string" // topik untuk MQTT data
+#define MQTT_TOPIC "$aws/things/hello/shadow/update" // topik untuk MQTT data
 #define AWS_HOST "a1fif474smypl7-ats.iot.us-west-2.amazonaws.com"
 AWS_IOT aws;
 #define ECPin 35  
@@ -17,10 +17,21 @@ unsigned long previous_millis = 0;
 unsigned long detik;
 unsigned long baca;
 unsigned long publish;
-int ecVol;
-int ec;
+float ecVol;
+float ec;
+float adcEC;
 
-int adcEC=0; 
+int status = WL_IDLE_STATUS;
+int tick=0,msgCount=0,msgReceived = 0;
+char payload[512];
+char rcvdPayload[512];
+
+void mySubCallBackHandler (char *topicName, int payloadLen, char *payLoad)
+{
+    strncpy(rcvdPayload,payLoad,payloadLen);
+    rcvdPayload[payloadLen] = 0;
+    msgReceived = 1;
+}
 
 void connectToWiFi()
 {
@@ -43,14 +54,26 @@ void connectToWiFi()
 }
 void connectToAWS()
 {
-  Serial.println("\n  INISIALISASI KONEKSI KE AWS....");
-  if (aws.connect(AWS_HOST, CLIENT_ID) == 0) { // MENGKONEKSIKAN KE HOST & returns 0 JIKA SUKSES TERKONEKSI
-    Serial.println("  SUDAH TERKONEKSI KE AWS\n");
-  }
-  else {
-    Serial.println("  GAGAL TERKONEKSI!\n PASTIKAN LAGI SUBSCRIPTION KE MQTT DI HALAMAN TEST (TEST PAGE)");
-  }
-  Serial.println("  READY");
+  if(aws.connect(AWS_HOST,CLIENT_ID)== 0)
+    {
+        Serial.println("Connected to AWS");
+        delay(1000);
+
+        if(0==aws.subscribe(MQTT_TOPIC,mySubCallBackHandler))
+        {
+            Serial.println("Subscribe Successfull");
+        }
+        else
+        {
+            Serial.println("Subscribe Failed, Check the Thing Name and Certificates");
+            while(1);
+        }
+    }
+    else
+    {
+        Serial.println("AWS connection failed, Check the HOST Address");
+        while(1);
+    }
 }
 
 void sendJsonToAWS()
@@ -63,11 +86,19 @@ void sendJsonToAWS()
   reportedObj["EC"] = ec ;
   
   // Publish the message to AWS
-  char jsonBuffer[512];
-  serializeJson(jsonDoc, jsonBuffer);
-  aws.publish(MQTT_TOPIC, jsonBuffer);
+  char payload[512];
+  serializeJson(jsonDoc, payload);
   serializeJson(jsonDoc, Serial);
+  if (aws.publish(MQTT_TOPIC, payload) == 0) { // MEM-PUBLISH payload DAN returns 0 JIKA SUKSES
+      Serial.println("SUKSES\n");
+    }
+    else {
+      Serial.println("GAGAL!\n");
+    }
+    Serial.println(payload);
 }
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -76,6 +107,13 @@ void setup() {
 }
 
 void loop() {
+  if(msgReceived == 1)
+    {
+        msgReceived = 0;
+        Serial.print("Received Message:");
+        Serial.println(rcvdPayload);
+    }
+  
   current_millis = millis();
   if (current_millis - previous_millis > interval) {
     previous_millis = current_millis;
@@ -87,36 +125,14 @@ void loop() {
     adcEC = analogRead(ECPin);
     ecVol= adcEC *(3.3 / 4095.0);
     ec = (609.0 * ecVol) - 31.68;
-   // Serial.println(ec);
+    Serial.println(ec);
   }
-  if (publish+300 == detik) {
+  if (publish+120 == detik) {
     detik = 0;
     baca = detik;
     publish = detik;
     Serial.println("Publishing:- ");
     sendJsonToAWS();
   }
-//  client.loop();
-//  String ec_ = "EC: ";
-// 
-//  
-//  ec_ += String(ec);
-//  ec_ += "ppm ";
-//  
-//  char payload[40];
-//  ec_.toCharArray(payload, 40);
-//
-//  if (publish+300 == detik) {
-//    detik = 0;
-//    baca = detik;
-//    publish = detik;
-//    Serial.println("Publishing:- ");
-//    Serial.println(payload);
-//    if (aws.publish(MQTT_TOPIC, payload) == 0) { // MEM-PUBLISH payload DAN returns 0 JIKA SUKSES
-//      Serial.println("SUKSES\n");
-//    }
-//    else {
-//      Serial.println("GAGAL!\n");
-//    }
-  }
-//}
+
+}
